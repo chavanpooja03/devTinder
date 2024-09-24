dc6bf41eaaf7f29a87715dc7097c0fa16528a586
pujac <pujachavan789@gmail.com>
2024-09-24 15:28:06 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const express=require("express");
 const app=express();
-app.use("/test",(req,res)=>{
+
+app.get("/te+st/:testId/:password",(req,res)=>{
+    console.log(req.params);
     res.send("Testing successfully completed");
-})
+});
+app.use("/",(req,res)=>{
+    res.send("Simple route called");
+    })
 app.listen(3000,()=>{
  console.log("Server is listening");
 });
